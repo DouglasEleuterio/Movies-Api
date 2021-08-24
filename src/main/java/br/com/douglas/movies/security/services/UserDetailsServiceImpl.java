21ad069922db88ec7be2a47683b17f0fa7c8ea25
Douglas <douglasferreira@hdregistros.com.br>
2021-08-24 18:05:13 -0300

    Implementação do Security

---
+++ src/main/java/br/com/douglas/movies/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.douglas.movies.security.services;
+
+import br.com.douglas.movies.models.User;
+import br.com.douglas.movies.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
