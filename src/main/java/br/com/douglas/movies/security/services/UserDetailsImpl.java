eab45de83f630273b1c136377e1516d3100383ad
Douglas <douglasferreira@hdregistros.com.br>
2021-08-24 18:34:41 -0300

    Correção do 'return null'

--- src/main/java/br/com/douglas/movies/security/services/UserDetailsImpl.java
+++ src/main/java/br/com/douglas/movies/security/services/UserDetailsImpl.java
@@ -47,14 +47,22 @@ public class UserDetailsImpl implements UserDetails {
         return authorities;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
